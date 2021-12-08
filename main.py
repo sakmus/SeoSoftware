e39e28f11e509ad93bee410bcfd451a46d163bdb
Sakeef Mushfique <94451911+sakmus@users.noreply.github.com>
2021-12-08 09:53:41 +0000

    Added full SEO support for the title tags.

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ try:
   data = BeautifulSoup(html, "html.parser")
 
   # Functions
-  def key_title(keyword, data):
-    return (keyword in data.title.text.casefold()) if data.title else None
-
-  def stop_title(data):
+  def check_title(keyword, data):
     if data.title:
+      # Check Keyword
+      print("+ Keyword found in title." if keyword in data.title.text.casefold() else "- Keyword is not in title.")
+
+      # Check stop words
       words = 0
       ls_words = []
       with open('stopwords.txt', 'r') as f:
@@ -23,12 +24,16 @@ try:
           if re.search('r\b' + line.rstrip('\n') + r'\b', data.title.text.casefold()):
             words += 1
             ls_words.append(line.rstrip('\n'))
-      return f"{words} stop word(s) found in title. Consider removing {ls_words}" if words else "No stop word found"
+      print("- {words} stop word(s) found in title. Try removing: {ls_words}" if words else "+ No stop word found in title.")
 
+      # Check length
+      leng = len(data.title.text)
+      if leng > 49 and leng < 61: print("+ Title is fit by length.")
+      elif leng > 60: print("- Title is too big. Trim it down to 60.")
+      else: print("- Title is too short. Add some characters to make it more than 50.")
 
   # Recall the functions
-  print(key_title(keyword, data))
-  print(stop_title(data))
+  check_title(keyword, data)
 
 except:
   print("Failed to process the URL :(. The url might be broken.")
